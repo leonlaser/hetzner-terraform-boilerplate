217d4020eaf8104c2ad2ed5c601406cb302e31ef
Leon Dietsch <hallo@leondietsch.de>
2026-03-08 15:03:01 +0100

    feat(backend): add dummy database data to test backup and restore

--- docker/example-services/backend/index.js
+++ docker/example-services/backend/index.js
@@ -1,23 +1,70 @@
-import { SQL } from "bun";
+import {SQL} from "bun";
 
 // bun uses DATABASE_URL by default to connect to the database
 const sql = new SQL({
   connectionTimeout: 2,
 });
 
+// Create a demo tables for testing backup and restore
+await sql`
+  CREATE TABLE IF NOT EXISTS users
+  (
+    id    SERIAL PRIMARY KEY,
+    name  VARCHAR NOT NULL,
+    email VARCHAR NOT NULL
+  )
+`;
+
+await sql`
+  INSERT INTO users (id, name, email)
+  VALUES (1, 'Max Musterman', 'max@musterman.de')
+  ON CONFLICT (id) DO NOTHING
+`;
+
+
+await sql`
+  INSERT INTO users (id, name, email)
+  VALUES (2, 'Maxine Musterwoman', 'maxine@musterwoman.de')
+  ON CONFLICT (id) DO NOTHING
+`;
+
+await sql`
+  CREATE TABLE IF NOT EXISTS key_val
+  (
+    key VARCHAR PRIMARY KEY,
+    val VARCHAR NOT NULL
+  )`;
+
+await sql`
+  INSERT INTO key_val
+  VALUES ('a', '1'),
+         ('b', '2'),
+         ('c', '3')
+  ON CONFLICT (key) DO NOTHING
+`
+
+// Example server to test and demonstrate the backend 
 const server = Bun.serve({
   port: 3000,
   routes: {
    "/api": async () => {
      try {
-       const query = sql`SELECT NOW() AS current_time`.execute();
-       setTimeout(() => query.cancel(), 100);
+       const query1 = sql`SELECT NOW() AS "current_time"`.execute();
+       const query2 = sql`SELECT *
+                          FROM users`.execute();
+       const query3 = sql`SELECT *
+                          FROM key_val`.execute();
+
+       setTimeout(() => query1.cancel(), 100);
+       setTimeout(() => query2.cancel(), 100);
+       setTimeout(() => query3.cancel(), 100);
+       
        return Response.json({
          env: {
            GLOBAL_VARIABLE: process.env.GLOBAL_VARIABLE,
            ENVIRONMENT_SPECIFIC_VARIABLE: process.env.ENVIRONMENT_SPECIFIC_VARIABLE,
          },
-         data: await query,
+         data: [...(await query1), ...(await query2), ...(await query3)],
        });
      } catch (error) {
        return Response.json({ error: error.message }, { status: 500 });
